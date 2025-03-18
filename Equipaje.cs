c78fda77589ab74f7582be71dbc0c638dd074685
Ydominguez06 <yelson.dominguez@ujcv.edu.hn>
2025-03-18 10:18:00 -0600

    Add files via upload

---
+++ Equipaje.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_Aeropuerto_Grupo_2.Objetos
+{
+    class Equipaje
+    {
+        public string IdMaleta { get; set; }
+        public string Propietario { get; set; }
+        public double Peso { get; set; }
+        public string Color { get; set; }
+        public string Material { get; set; }
+        public string Marca { get; set; }
+        public  string Prioridad { get; set; }
+    
+     public Equipaje(string idMaleta, string propietario, double peso, string color, string material, string marca , string prioridad)
+     {
+            IdMaleta = idMaleta;
+            Propietario = propietario;
+            Peso = peso;
+            Prioridad = prioridad;
+            Marca = marca;
+            Color = color;
+            Material = material;
+
+     }
+
+        public void MostrarMaleta() => Console.WriteLine($"Maleta {IdMaleta}: Propietario {Propietario}, Peso {Peso} kg, Color {Color}");
+        public void CambiarPeso(double nuevoPeso) => Peso = nuevoPeso;
+    }
+
+}
---
+++ Factura.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_Aeropuerto_Grupo_2.Objetos
+{
+    internal class Factura
+    {
+        public int NumeroFactura { get; set; }
+        public string Cliente { get; set; }
+        public string FechaCreacion { get; set; }
+        public double MontoTotal { get; set; }
+        public string MetodoPago { get; set; }
+        public string Concepto { get; set; }
+        public bool Pagado { get; set; }
+
+
+        public Factura(int numeroFactura, string cliente, string fechaCreacion, double montoTotal, string metodoPago, string concepto, bool pagado)
+        {
+            NumeroFactura = numeroFactura;
+            Cliente = cliente;
+            FechaCreacion = fechaCreacion;
+            MontoTotal = montoTotal;
+            MetodoPago = metodoPago;
+            Concepto = concepto;
+            Pagado = pagado;
+        }
+
+        public void MostrarFactura() => Console.WriteLine($"Factura {NumeroFactura}: Cliente {Cliente}, Monto: {MontoTotal}, Pagado: {Pagado}");
+        public void AplicarPago() => Pagado = true;
+        public void ModificarMonto(double nuevoMonto) => MontoTotal = nuevoMonto;
+    }
+}
